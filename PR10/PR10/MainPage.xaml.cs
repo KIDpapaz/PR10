2461dd08b140edacc2109cf7a88a1eec472ce176
Kit <Kit@DESKTOP-D1TNP2C>
2021-09-28 10:50:53 +0300

    Добавьте файлы проекта.

---
+++ PR10/PR10/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PR10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked_trx(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
